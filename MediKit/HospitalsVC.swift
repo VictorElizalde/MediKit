f04b5e745853152dc2cac4aa00480f139d0e06c7
Victor Elizalde <vic-bull@hotmail.com>
2017-04-17 11:43:52 -0500

    Info View finished

--- MediKit/HospitalsVC.swift
+++ MediKit/HospitalsVC.swift
@@ -1,8 +1,8 @@
 //
-//  SecondViewController.swift
+//  HospitalsVC.swift
 //  MediKit
 //
-//  Created by Victor Elizalde on 3/18/17.
+//  Created by Victor Elizalde on 4/17/17.
 //  Copyright © 2017 Victor Elizalde. All rights reserved.
 //
 
@@ -12,14 +12,24 @@ class HospitalsVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
 
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
 
 }
-
--- MediKit/InfoVC.swift
+++ MediKit/InfoVC.swift
@@ -10,9 +10,21 @@ import UIKit
 
 class InfoVC: UIViewController {
 
+    @IBOutlet weak var empresaLabel: UILabel!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var misionLabel: UILabel!
+    @IBOutlet weak var visionLabel: UILabel!
+    @IBOutlet weak var nosotrosLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        empresaLabel.font = UIFont.boldSystemFont(ofSize: 17.0)
+        titleLabel.font = UIFont.boldSystemFont(ofSize: 20.0)
+        misionLabel.font = UIFont.boldSystemFont(ofSize: 17.0)
+        visionLabel.font = UIFont.boldSystemFont(ofSize: 17.0)
+        nosotrosLabel.font = UIFont.boldSystemFont(ofSize: 17.0)
+        
     }
 
     override func didReceiveMemoryWarning() {
